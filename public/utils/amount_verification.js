1f0a594e9ad9f1beca25e9528bccae8ee0c2bb01
Jordan <ivanosky_617@hotmail.com>
2025-03-18 20:04:51 +0000

    Refactor bn amount_verification.js

--- public/utils/amount_verification.js
+++ public/utils/amount_verification.js
@@ -1,14 +1,20 @@
-const { BN, BN_ZERO, BN_ONE } = polkadotUtil;
+const { BN, BN_ZERO } = polkadotUtil;
 
 import { MIN_BAL_FREE } from '../constants.js';
 import { balances } from '../subscribe_balances.js';
 
 //Function to Validate Amount
-export function validateAmount(amount,currency){
+export function validateAmount(amount, currency){
 
-    if (isNaN(amount) || amount <= 0 || balances[currency] < amount + MIN_BAL_FREE[currency]){ 
-      return false; 
-      } 
-      
+  // Verify amount is a valid BN and greater than 0
+   if (!BN.isBN(amount) || amount.lte(BN_ZERO)) { 
+    return false; 
+} 
+
+ // Validate required balance
+ if (balances[currency].lt(amount.add(MIN_BAL_FREE[currency]))) {
+  return false;
+} 
+
   return true;
   }
