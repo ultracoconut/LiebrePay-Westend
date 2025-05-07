8b24aae100feda40f93c4195b0697d0f73ced1f1
Jordan <ivanosky_617@hotmail.com>
2025-05-07 12:42:23 +0100

    update code update_history.js

--- public/payment_history/update_history.js
+++ public/payment_history/update_history.js
@@ -3,32 +3,36 @@ import { fetchTransfers } from './fetch.js';
 import { paymentList } from './list.js';
 
 export async function updateHistory(page) {
+    
+    //DOM Elements
     const message = document.getElementById('history-message');
     const listContainer = document.getElementById('transferList');
     
-    if (!message || !listContainer) return;
+    if (!message || !listContainer) return null;
 
     if (!account) {
         message.textContent = 'Connect Wallet to show payment history';
         listContainer.innerHTML = '';
-        return;
+        return null;
     }
 
     try {
         message.textContent = 'Loading...';
 
-        const transfers = await fetchTransfers(account.address, page);
+        const data = await fetchTransfers(account.address, page);
+        const  transfers = data.transfers;
         
         if (!transfers || transfers.length === 0) {
             message.textContent = 'No payment history available for this wallet.';
             listContainer.innerHTML = '';
-            return;
+            return null;
         }
 
         paymentList(transfers, listContainer);
 
         //Limpiar mensaje al finalizar
         message.textContent = '';
+        return data.count;
 
         
     } catch (error) {
