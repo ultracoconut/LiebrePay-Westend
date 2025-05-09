a78860988a97beda97eab53573667723c15d96c9
Jordan <ivanosky_617@hotmail.com>
2025-05-09 20:27:23 +0100

    add update_controls.js

---
+++ public/update_ui/update_controls.js
@@ -0,0 +1,16 @@
+import { paginationState } from '../payment_history/pagination_state.js';
+
+export function updateControls(state = 'flex') {
+
+    //Dom Elements
+    const prevBtn = document.getElementById('prev-page-btn');
+    const nextBtn = document.getElementById('next-page-btn');
+    const pageDisplay = document.getElementById('page-display');
+    const container = document.getElementById('pagination');
+
+    pageDisplay.textContent = `${paginationState.currentPage + 1} of ${paginationState.totalPages}`;
+    prevBtn.disabled = paginationState.currentPage === 0;
+    nextBtn.disabled = paginationState.currentPage >= paginationState.totalPages - 1;
+    container.style.display = state;
+ 
+    }
