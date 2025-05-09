fd0deaef34d0f94d73dabc82461fb77d5f96e043
Jordan <ivanosky_617@hotmail.com>
2025-05-09 20:33:52 +0100

    update code init_payment_history.js

--- public/init_pages/init_payment_history.js
+++ public/init_pages/init_payment_history.js
@@ -1,15 +1,44 @@
 import { paymentHistoryController } from '../payment_history/payment_history_controller.js';
+import { updateHistory } from '../payment_history/update_history.js';
+import { updateControls } from '../update_ui/update_controls.js';
+import { paginationState } from '../payment_history/pagination_state.js';
+
 
 export function initPaymentHistory(){
+   
+   //DOM elements
+   const prevBtn = document.getElementById('prev-page-btn');
+   const nextBtn = document.getElementById('next-page-btn');
+   
+   //Setup buttons
+   prevBtn.addEventListener('click', async () => {
+    try {
 
-    paymentHistoryController();
+      if (paginationState.currentPage > 0) {
+          paginationState.currentPage--;
+          await updateHistory(paginationState.currentPage);
+          updateControls();
+      }
+
+     } catch (error) {
+      console.error('Error loading previous page:', error);
+       }
+   });
+
+
+  nextBtn.addEventListener('click', async () => {
+    try {
+      
+      if (paginationState.currentPage < paginationState.totalPages - 1) {
+          paginationState.currentPage++;
+          await updateHistory(paginationState.currentPage);
+          updateControls();
+      }
+     } catch (error) {
+      console.error('Error loading next page:', error);
+       }
+  });
+
+    //Call paymentHistory Controller at start
+     paymentHistoryController();
 }
-
-
-
-
-
-    
- 
- 
-  
