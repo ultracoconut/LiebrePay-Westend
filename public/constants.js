c882b7337bb1f2aa94359a56e535d564f163c973
Jordan <ivanosky_617@hotmail.com>
2025-05-19 18:26:52 +0100

    Add MIN_PAY_AMOUNT and improve code constants.js

--- public/constants.js
+++ public/constants.js
@@ -1,32 +1,46 @@
 export const ASSETS_ID = {
-  UCOCO: 333,
-  COCOUSD: 400,
-};
+    UCOCO: 333,
+    COCOUSD: 400,
+  };
 
 export const MAX_ROWS = 100;
 export const URL_PROVIDER = 'wss://westend-asset-hub-rpc.polkadot.io';
 export let MIN_BAL_FREE = null;
+export let MIN_PAY_AMOUNT = null;
 
 //Initializes constants requiring BN from polkadotUtil
 export function initializeConstants() {
 
-try{
+ try{
+ 
+ if (!polkadotUtil?.BN) {
+    throw new Error("polkadotUtil or BN is not available");
+  }
+  
+  const BN = polkadotUtil.BN;
+  
+  //Decimal precision (bn)
+  const DEC_PREC = new BN(10).pow(new BN(12));
+  
+  //Minimum free balance required in LiebrePay westend (bn)
+  MIN_BAL_FREE = { 
+    WND: DEC_PREC.muln(2).divn(10), //0.2
+    UCOCO: DEC_PREC.muln(2).divn(10), //0.2
+    COCOUSD: DEC_PREC.muln(2).divn(10), //0.2
+  };
 
-if (!polkadotUtil?.BN) {
-  throw new Error("polkadotUtil or BN is not available");
-}
+//Minimum amount to pay in LiebrePay (bn)
+ MIN_PAY_AMOUNT = {
+    WND: DEC_PREC.divn(10), //0.1
+    UCOCO: DEC_PREC.divn(10), //0.1
+    COCOUSD: DEC_PREC.divn(10), //0.1
+  };
 
-const BN = polkadotUtil.BN;
-const DEC_PREC = new BN(10).pow(new BN(12));
-MIN_BAL_FREE = { 
-  WND: DEC_PREC.div(new BN(10)),
-  UCOCO: DEC_PREC.div(new BN(10)),
-  COCOUSD: DEC_PREC.div(new BN(10)),
-};
-return true;
+  return true;
 
-}catch(error){
-console.error("Error initializing constants:", error);
-return false;
+  }catch(error){
+  console.error("Error initializing constants:", error);
+  return false;
+  }
 }
-}
+ 
