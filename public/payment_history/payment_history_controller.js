8d275e022f9ecd692e2737bc10d49736ad333b14
Jordan <ivanosky_617@hotmail.com>
2025-05-24 20:19:16 +0100

    Add total payments top-bar to payment history page+ ui improvements

--- public/payment_history/payment_history_controller.js
+++ public/payment_history/payment_history_controller.js
@@ -1,6 +1,7 @@
 import { updateHistory } from './update_history.js';
 import { updateControls } from '../update_ui/update_controls.js';
 import { paginationState } from './pagination_state.js';
+import { updateTotalPayments } from '../update_ui/update_total_payments.js';
 
 export async function paymentHistoryController() {  
 
@@ -8,9 +9,9 @@ export async function paymentHistoryController() {
     const prevBtn = document.getElementById('prev-page-btn');
     const nextBtn = document.getElementById('next-page-btn');
     const pageDisplay = document.getElementById('page-display');
-    const container = document.getElementById('pagination');
+    const phContainer = document.getElementById('ph-container');
 
-    if(!container) return;
+    if(!phContainer) return;
 
     //Clear pagination state
     paginationState.currentPage = 0;
@@ -24,19 +25,21 @@ export async function paymentHistoryController() {
     nextBtn.disabled = true;
     
     //Hide container at start
-    container.style.display = 'none';
+    phContainer.style.display = 'none';
 
   try {
 
     //Initial call to fetch transfers and set up pagination
     const transferCount = await updateHistory(0); //Display first page and retrieve transfer count
+    
     if (!transferCount){
-     updateControls('none');
-     return;
+      return;
     } 
 
+    phContainer.style.display = 'block';
     paginationState.totalPages = Math.ceil(transferCount / 20);
     updateControls();
+    updateTotalPayments(transferCount);
 
    } catch (error) {
       console.error('Error in Payment history controller:', error);
