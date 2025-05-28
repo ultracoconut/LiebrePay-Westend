a497562f210ea719ca78e5515e4747f0b2601873
Jordan <ivanosky_617@hotmail.com>
2025-05-28 19:46:18 +0100

    improve ui account info

--- public/update_ui/update_account_info.js
+++ public/update_ui/update_account_info.js
@@ -2,25 +2,30 @@ import { balances } from '../subscribe_balances.js';
 import { account } from '../connect_wallet.js';
 import { formatConversionOut } from '../utils/format_conversion_output.js';
 
-export  function updateAccountInfo(){
-   
+export function updateAccountInfo() {
+  
+  try {
    const address = document.getElementById('address-info');
 
-   if(!address){//Verify account info page
-   return;
-   }
+   //Verify account info page
+   if (!address) return;
 
-   //DOM elements
-   const balanceFreeWND = document.getElementById('balance-free-WND-info');
-   const balanceResWND = document.getElementById('balance-reserved-WND-info');
-   const balanceUCOCO = document.getElementById('balance-UCOCO-info');
-   const balanceCOCOUSD = document.getElementById('balance-COCOUSD-info');
+    //DOM elements
+    const name = document.getElementById('name-info');
+    const balanceFreeWND = document.getElementById('balance-free-WND-info');
+    const balanceResWND = document.getElementById('balance-reserved-WND-info');
+    const balanceUCOCO = document.getElementById('balance-UCOCO-info');
+    const balanceCOCOUSD = document.getElementById('balance-COCOUSD-info');
 
-   //Display formatted balances
-   address.textContent = account ? account.address : 'Not selected account';
-   balanceFreeWND.textContent = account ? `Free: ${formatConversionOut(balances['WND'], 12)}` : 'Not available';
-   balanceResWND.textContent = account ? `Reserved: ${formatConversionOut(balances['WNDRes'], 12)}` : 'Not available';
-   balanceUCOCO.textContent = account ? formatConversionOut(balances['UCOCO'], 12) : 'Not available';
-   balanceCOCOUSD.textContent = account ? formatConversionOut(balances['COCOUSD'], 12) : 'Not available';
+    //Update content
+    name.textContent = account?.meta?.name || 'Not selected account';
+    address.textContent = account?.address || 'Not selected account';
+    balanceFreeWND.textContent = account ? `Free: ${formatConversionOut(balances['WND'], 12)}` : 'Not available';
+    balanceResWND.textContent = account ? `Reserved: ${formatConversionOut(balances['WNDRes'], 12)}` : 'Not available';
+    balanceUCOCO.textContent = account ? formatConversionOut(balances['UCOCO'], 12) : 'Not available';
+    balanceCOCOUSD.textContent = account ? formatConversionOut(balances['COCOUSD'], 12) : 'Not available';
 
+  } catch (error) {
+    console.error('Error updating account info:', error);
+  }
 }
