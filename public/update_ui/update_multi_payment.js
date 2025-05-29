23299817d387ca5106b3b63190132f824f1112fd
Jordan <ivanosky_617@hotmail.com>
2025-05-29 17:19:22 +0100

    improve code update_multi_payment.js

--- public/update_ui/update_multi_payment.js
+++ public/update_ui/update_multi_payment.js
@@ -1,25 +1,26 @@
 import { account } from '../connect_wallet.js';
 
-export function updateMultiPayment(){
-
+export function updateMultiPayment() {
+  try {
     //DOM elements
     const selBut = document.getElementById('select-csv-button');
-    
-    if (!selBut) {
-        return; //Verify multi payment page
+
+    if (!selBut) {// Verify multi payment page
+      return; 
     }
-     
+
     //Disable the button if no account is connected
     if (!account) {
-        selBut.disabled = true;
-        selBut.textContent = 'Please connect a Wallet';
-        return;
+      selBut.disabled = true;
+      selBut.textContent = 'Please connect a Wallet';
+      return;
     }
 
     //Enable the button if account connected
     selBut.disabled = false;
     selBut.textContent = '📤 Upload .csv File';
 
+  } catch (error) {
+    console.error('Error updating multi payment ui:', error);
+  }
 }
-
-
