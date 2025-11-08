1ebbef4ae91f0058b405b8ebcc4b779ca7b3cef7
ultracoconut <ivanosky_617@hotmail.com>
2025-11-08 18:48:37 +0100

    update file update_account_info.js with locked balance info

--- public/update_ui/update_account_info.js
+++ public/update_ui/update_account_info.js
@@ -15,6 +15,7 @@ export function updateAccountInfo() {
     const name = document.getElementById('name-info');
     const balanceFreeWND = document.getElementById('balance-free-WND-info');
     const balanceResWND = document.getElementById('balance-reserved-WND-info');
+    const balanceLockWND = document.getElementById('balance-locked-WND-info');
     const balanceUCOCO = document.getElementById('balance-UCOCO-info');
     const balanceCOCOUSD = document.getElementById('balance-COCOUSD-info');
     
@@ -23,6 +24,7 @@ export function updateAccountInfo() {
     address.textContent = account?.address || 'Not selected account';
     balanceFreeWND.textContent = account ? `Free: ${formatConversionOut(balances['WND'], DECIMAL['WND'])}` : 'Not available';
     balanceResWND.textContent = account ? `Reserved: ${formatConversionOut(balances['WNDRes'], DECIMAL['WND'])}` : 'Not available';
+    balanceLockWND.textContent = account ? `Locked: ${formatConversionOut(balances['WNDLock'], DECIMAL['WND'])}` : 'Not available';
     balanceUCOCO.textContent = account ? formatConversionOut(balances['UCOCO'], DECIMAL['UCOCO']) : 'Not available';
     balanceCOCOUSD.textContent = account ? formatConversionOut(balances['COCOUSD'], DECIMAL['COCOUSD']) : 'Not available';
 
