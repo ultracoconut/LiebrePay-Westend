136db5f3891fcfa48ebf785cb3f21a3c38d36f0a
ultracoconut <ivanosky_617@hotmail.com>
2025-11-19 17:57:44 +0100

    update single_payment_assets.js

--- public/transactions/single_payment_assets.js
+++ public/transactions/single_payment_assets.js
@@ -3,6 +3,7 @@ import { MIN_BAL_FREE, ASSETS_ID, MULTILOCATION, DECIMAL } from '../constants.js
 import { balances } from '../subscribe_balances.js';
 import { apiAH, initializeApi } from '../init_apis.js';
 import { formatConversionOut } from '../utils/format_conversion_output.js';
+import { customConfirm } from '../utils/ui/custom_confirm.js';
 
 
 export async function singlePaymentAssets (currency, account, injector, destination, value) {
@@ -47,7 +48,7 @@ export async function singlePaymentAssets (currency, account, injector, destinat
       let {partialFee:txFee} = await extrinsic.paymentInfo(account);
   
       //Confirmation message
-      let userConfirmed = confirm(`Please, confirm payment of ${formatConversionOut (value, DECIMAL[currency])} ${currency} to beneficiary ${destination}
+      const userConfirmed = await customConfirm(`Please, confirm payment of ${formatConversionOut (value, DECIMAL[currency])} ${currency} to beneficiary ${destination}.\n
       Estimated fee: ${formatConversionOut(txFee, DECIMAL['WND'])} WND`);
     
       //User cancel transaction
